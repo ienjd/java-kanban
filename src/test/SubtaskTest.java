268c98bda7709c27155b5d5ac02863f39b3b75ba
Vladislav Shutov <coldhobbit31@ya.ru>
2025-07-13 16:33:17 +0300

    bugfix: убрать пробел в наименование тестового метода subtask

--- src/test/SubtaskTest.java
+++ src/test/SubtaskTest.java
@@ -7,7 +7,7 @@ import tasks.Subtask;
 class SubtaskTest {
 
     @Test//экземпляры Subtask равны друг другу, если равны их id, а также, объект Subtask нельзя сделать своим эпиком
-    void subtaskEqualsSubtask() {
+    void subtaskEqualsSubtask(){
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Subtask firstSubtask = inMemoryTaskManager.createSubtask("Первая подзадача", "Описание первой подзадачи", 1);
         Subtask secondSubtask = firstSubtask;
