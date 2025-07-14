720789e6ee520f498aad42084ee3e28b14130d68
Vladislav Shutov <coldhobbit31@ya.ru>
2025-07-14 11:12:26 +0300

    bugfix: исправлен тестовый метод класса Subtask

--- test/SubtaskTest.java
+++ test/SubtaskTest.java
@@ -7,7 +7,7 @@ import tasks.Subtask;
 class SubtaskTest {
 
     @Test
-//экземпляры Subtask равны друг другу, если равны их id, а также, объект Subtask нельзя сделать своим эпиком
+//экземпляры Subtask равны друг другу, если равны их id
     void subtaskEqualsSubtask() {
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager();
         Subtask firstSubtask = inMemoryTaskManager.createSubtask("Первая подзадача", "Описание первой подзадачи", 1);
@@ -16,8 +16,5 @@ class SubtaskTest {
         Assertions.assertEquals(firstSubtask.getId(), secondSubtask.getId());
         Assertions.assertEquals(firstSubtask, secondSubtask);
 
-        secondSubtask.setEpicId(1);
-        Assertions.assertNotEquals(1, secondSubtask.getEpicId());
     }
-
 }
