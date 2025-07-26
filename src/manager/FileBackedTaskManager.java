d4e0a2919efa99a9c49ab757b1aa7a8843833257
Vladislav Shutov <coldhobbit31@ya.ru>
2025-07-26 09:08:20 +0300

    feat: переопределены унаследованные методы, создан метод save()

--- src/manager/FileBackedTaskManager.java
+++ src/manager/FileBackedTaskManager.java
@@ -1,5 +1,49 @@
 package manager;
 
-public class FileBackedTaskManager extends InMemoryTaskManager{
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
 
+import java.util.HashMap;
+
+public class FileBackedTaskManager extends InMemoryTaskManager{
+    String path;
+
+    public FileBackedTaskManager(String path){
+        this.path = path;
+    }
+
+    private void save(){
+
+    }
+
+    @Override
+    public void addTaskToList(Task task, HashMap hashMap) {
+        super.addTaskToList(task, hashMap);
+        save();
+    }
+
+    @Override
+    public void deleteTaskFromList(int id) {
+        super.deleteTaskFromList(id);
+        save();
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        super.updateTask(task);
+        save();
+    }
+
+    @Override
+    public void updateEpic(Epic epic) {
+        super.updateEpic(epic);
+        save();
+    }
+
+    @Override
+    public void updateSubtask(Subtask subtask) {
+        super.updateSubtask(subtask);
+        save();
+    }
 }
