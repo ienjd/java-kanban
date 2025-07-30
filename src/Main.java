4d4c53674f74bd2b53e27a5890e15575d63a501c
Vladislav Shutov <coldhobbit31@ya.ru>
2025-07-30 16:39:51 +0300

    bugfix: теперь точно удалены

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import manager.FileBackedTaskManager;
-import manager.InMemoryTaskManager;
-import tasks.Epic;
-import tasks.Status;
-import tasks.Subtask;
-import tasks.Task;
-import ui.MainUI;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public void main(String[] args) throws IOException {
