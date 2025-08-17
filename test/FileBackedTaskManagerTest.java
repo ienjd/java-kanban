d975177f252c0e011780a7eda1be0bc62fbda58d
Влад Шутов <coldhobbit31@yandex.ru>
2025-08-17 20:15:14 +0300

    feat: отправить в влсьмую итерацию

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -5,14 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 class FileBackedTaskManagerTest{
-    FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager
-            (new File("C:\\Users\\coldh\\IdeaProjects\\java-kanban\\fileForSavingTasks\\file.csv"));
-
-
-    @Test
-    void fileIsExist(){
-        Assertions.assertTrue(fileBackedTaskManager.file.exists());
-    }
 
     @Test
     public void testException() {
