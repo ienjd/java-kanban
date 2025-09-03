525090b52a5e2bfa4773a3adcbe0c0bc362ff355
Влад Шутов <coldhobbit31@yandex.ru>
2025-09-03 18:08:44 +0300

    feat: отправить в седьмую итерацию тестов

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -16,7 +16,7 @@ public class Epic extends Task {
         this.startTime = LocalDateTime.of(2021, 1, 1, 1, 1);
     }
 
-    public void clearSubtasksList(){
+    public void clearSubtasksList() {
         subtasks.clear();
     }
 
